4769a07aca54a36d5a38d120b1035d1a4100c111
Ilya Semenov <ilya@semenov.co>
2023-11-09 11:10:43 +0700

    Add loading status to playground

--- playground/app.vue
+++ playground/app.vue
@@ -4,8 +4,15 @@ const counter = ref(0)
 function add() {
   counter.value++
 }
+
+const loading = ref(true)
+
+onMounted(() => {
+  loading.value = false
+})
 </script>
 
 <template>
+  <div>{{ loading ? "Loading..." : "Loaded" }}</div>
   <button @click="add">Click me ({{ counter }})</button>
 </template>
